5149374028a2bc0a20e3258c65bca12831c39fc2
Thomas Card <tcardlab@gmail.com>
2022-09-14 04:52:43 -0400

    auto-import and fix windi scanning

--- src/pages/Start/index.vue
+++ src/pages/Start/index.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script setup lang="ts">
-  import HelloWorld from '@/components/HelloWorld.vue'
+  //import HelloWorld from '@/components/HelloWorld.vue'
 
   import useStore from '@/stores/store'
   const $store = useStore()
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import WindiCSS from 'vite-plugin-windicss'
+import Components from 'unplugin-vue-components/vite'
 import { resolve } from "path"
+// import Inspect from 'vite-plugin-inspect'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    WindiCSS(),
+    Components({resolvers: []}),
     vue({ reactivityTransform: true }),
-    WindiCSS()
+    // Inspect()
   ],
   resolve: {
     extensions: ['.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
