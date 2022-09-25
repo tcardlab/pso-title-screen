6fcb40b4c870086d58c20e64b420aecc49c5fa5d
Thomas Card <tcardlab@gmail.com>
2022-09-25 06:40:54 -0400

    Feat: init alignment

--- src/pages/Sigil/index.vue
+++ src/pages/Sigil/index.vue
@@ -5,7 +5,7 @@
       <details>
         <summary> Display Port </summary>
 
-        <label>Offset:  {{offset}}</label> <button @click="tmp">reset</button> <br>
+        <label>Offset:  {{offset}}</label> <button @click="_offset">reset</button> <br>
         <input type="range"  min="-20" max="0" step="0.5" v-model="offset">
       </details>
 
@@ -16,15 +16,16 @@
         <input type="range"  min="-20" max="0" step="0.5" v-model="offset">
       </details>
      
+      {{viewBox}}
     </div>
 
-    <div class="display-port">
-      <img class="refImg" :src="refImg">
+    <div class="display-port"  ref="svg">
+      <img class="refImg" draggable="false" :src="refImg">
 
       <div class="svg-out">
         <jsxNode />
-        <svg viewBox="-394.5 -394.5 789 789">
-          <circle r="10px" x="50%" y="50%"/>
+        <svg :viewBox="viewBox">
+          <circle r="125px" x="50%" y="50%"/>
         </svg>
       </div>
     </div>
@@ -34,9 +35,19 @@
 <script lang="tsx" setup>
   import refImg from './PSO.png'
   import memRef from '@/use/memRef'
+  
 
-  let offset = memRef(-10)
-  const tmp = offset.reset
+  import { useElementSize } from '@vueuse/core'
+
+  let [offset, _offset] = memRef(-9.5)
+
+  let svg = $ref(null)
+  let { width: svgW, height: svgH } = $( $$(useElementSize(svg)) )
+  let viewBox = $computed(()=> {
+    return "-500 -500 1000 1000";
+    if (!svgH) return "-500 -500 1000 1000";
+    return `${-svgH/2} ${-svgH/2} ${svgH} ${svgH}`
+  })
 
   const jsxNode = () => <div style="color: orange">+</div>
 </script>
@@ -48,8 +59,9 @@
     stroke-width: 10px;
     fill: none;
 
+    overflow: visible;
     height: calc(100% + v-bind("(-1*offset)+'%'"));
-    width: 100%
+    width: auto
   }
 </style>
 
@@ -103,17 +115,21 @@
       align-self: center;
       justify-self: center;
       height: 100vh;
-      width: auto
+      //width: auto
     }
   }
 
   .refImg {
-    opacity: .5
+    opacity: .5;
+    z-index: 0;
   }
 
   .svg-out {
-    width: 100%;
+    //height: 100%;
+    //width: 1000px;
     display: grid;
+    z-index: 1;
+    overflow: visible;
 
     &> * {
       grid-area: 1 / 1;
--- src/use/memRef.ts
+++ src/use/memRef.ts
@@ -1,19 +1,24 @@
 import {Ref, ref, reactive} from 'vue'
 
-interface MemRef extends Ref {
+/* interface MemRef extends Ref {
   reset: ()=>any
-}
+} */
 
 /* const memRef = (value:any)=>{
   let rf = reactive({value, reset: ()=>{}})
   rf.reset = () => rf.value = value
   return rf //as MemRef
 }  */
-
+/* 
 const memRef = (value:any)=>{
   let rf = ref(value) as MemRef
   rf.reset = () => rf.value = value
   return rf
+}  */
+
+const memRef = (value:any)=>{
+  let rf = ref(value)
+  return [rf, () => rf.value = value]
 } 
 
 export default memRef
